e3b926779f69148eff4d599fdb0ecfa34476dbbc
ffmtw <67579722@qq.com>
2013-09-05 15:47:03 +0800

    非法进入添加提示

--- class/controller/installController.class.php
+++ class/controller/installController.class.php
@@ -8,7 +8,12 @@ class installController extends Controller{
 	{
 	//获取安装步骤ID
 	$install_step=$this->get_step_num();
-		if($install_step==1)
+		if($install_step==0)
+		{
+		// 显示非法进入
+		$this->show_error_enter();
+		}
+		elseif($install_step==1)
 		{
 		// 显示填写数据库信息的表单
 		$this->show_db_form();
@@ -23,6 +28,19 @@ class installController extends Controller{
 		// 显示安装已经成功
 		$this->show_install_ok();
 		}
+	}
+	public function get_step_num()
+	{
+	// 获取配置
+	$config=cfg_class::connect();
+	if(is_file(CONFIG_PATH.'install.lock'))
+	return 0;
+	elseif(!$config->get_var('set_db_ok'))
+	return 1;
+	elseif(!$config->get_var('set_admin_ok'))
+	return 2;
+	else
+	return 3;
 	}
 }
 ?>
