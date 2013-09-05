889e823d1105333decc1a962766dbe3230f01e14
ffmtw <67579722@qq.com>
2013-09-05 15:47:36 +0800

    修正语法错误

--- config/cfg.php
+++ config/cfg.php
@@ -1,8 +1,8 @@
 <?php
 // 系统配置文件
 $config=array(
-set_db_ok=>false,
-set_admin_ok=>false,
-oid_update_key=>'12sdas',
+'set_db_ok'=>false,
+'set_admin_ok'=>false,
+'oid_update_key'=>'12sdas',
 );
 ?>
