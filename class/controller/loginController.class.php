0127e2c5a4b4731024482d2b909432a1da11a2bd
qq67579722 <67579722@qq.com>
2013-09-06 18:53:48 +0800

    新增模版式控制器

---
+++ class/controller/loginController.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * 控制器：loginController
+ * 作者: 流光
+ * 登录页面控制器
+ * 时间:2013/9/6 18:46
+ */
+
+class loginController extends Controller {
+
+    public function runApp() {
+//....
+    }
+
+}
+
+?>
---
+++ class/controller/regController.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * 控制器：regController
+ * 作者: 流光
+ * 注册页面控制器
+ * 时间:2013/9/6 18:46
+ */
+
+class regController extends Controller {
+
+    public function runApp() {
+//....
+    }
+
+}
+
+?>
