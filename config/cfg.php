31574edaff0b27b5088b3adbddadbc0d35e7b061
qq67579722 <67579722@qq.com>
2013-09-06 18:44:27 +0800

    测试一下代码格式化

--- config/cfg.php
+++ config/cfg.php
@@ -1,10 +1,11 @@
 <?php
+
 // 系统配置文件
-$config=array(
-'set_db_ok'=>false,
-'install_db_ok'=>false,
-'set_admin_ok'=>false,
-'oid_update_key'=>'12sdas',
+$config = array(
+    'set_db_ok' => false,
+    'install_db_ok' => false,
+    'set_admin_ok' => false,
+    'oid_update_key' => '12sdas',
 );
 //配置数据库，安装数据库，配置管理员信息、、
 ?>
