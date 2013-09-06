0761f8dbdca63dff8acbcea92a55d77710f0c40a
liuguang <liuguang@liuguang-PC>
2013-09-06 15:18:48 +0800

    测试一下netbeans

--- autoload.php
+++ autoload.php
@@ -1,27 +1,29 @@
-<?php 
+<?php
+
 // 设置时区
 date_default_timezone_set('PRC');
 // 项目路径
-define('APP_PATH',realpath('.').DIRECTORY_SEPARATOR);
-/*定义类库目录*/
-define('CLASS_PATH',APP_PATH.'class'.DIRECTORY_SEPARATOR);
+define('APP_PATH', realpath('.') . DIRECTORY_SEPARATOR);
+/* 定义类库目录 */
+define('CLASS_PATH', APP_PATH . 'class' . DIRECTORY_SEPARATOR);
 // 配置文件路径
-define('CONFIG_PATH',APP_PATH.'config'.DIRECTORY_SEPARATOR);
+define('CONFIG_PATH', APP_PATH . 'config' . DIRECTORY_SEPARATOR);
 // 获取http地址
-define('WWW_ROOT','http://'.$_SERVER['HTTP_HOST'].substr($_SERVER['SCRIPT_NAME'],0,-9));
+define('WWW_ROOT', 'http://' . $_SERVER['HTTP_HOST'] . substr($_SERVER['SCRIPT_NAME'], 0, -9));
+
 //类的自动加载
-function __autoload($class_name)
-{
+function __autoload($class_name) {
 // 单独设置controller和view类的路径方便查找，调试
-if(substr($class_name,-10)=='Controller'&&($class_name!='Controller'))
-$class_path=CLASS_PATH.'controller'.DIRECTORY_SEPARATOR;
-elseif(substr($class_name,-4)=='View'&&($class_name!='View'))
-$class_path=CLASS_PATH.'view'.DIRECTORY_SEPARATOR;
+    if (substr($class_name, -10) == 'Controller' && ($class_name != 'Controller'))
+        $class_path = CLASS_PATH . 'controller' . DIRECTORY_SEPARATOR;
+    elseif (substr($class_name, -4) == 'View' && ($class_name != 'View'))
+        $class_path = CLASS_PATH . 'view' . DIRECTORY_SEPARATOR;
 // 其他类则在默认类库路径调用
-else
-$class_path=CLASS_PATH;
-$file_name=$class_path.$class_name.'.class.php';
-if(is_file($file_name))
-include $file_name;
+    else
+        $class_path = CLASS_PATH;
+    $file_name = $class_path . $class_name . '.class.php';
+    if (is_file($file_name))
+        include $file_name;
 }
+
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+
 //计时开始
-define('APP_START_TIME',microtime(true));
-/*包含引导文件*/
+define('APP_START_TIME', microtime(true));
+/* 包含引导文件 */
 include './autoload.php';
-/*运行*/
-$myweb=new webSite();
+/* 运行 */
+$myweb = new webSite();
 $myweb->runApp();
 ?>
